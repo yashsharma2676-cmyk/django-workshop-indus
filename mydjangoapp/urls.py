e03fc64657e2c9e387fb1f51ca32d1efc22690ef
yash <yashsharma2676@gmail.com>
2026-06-17 11:16:22 +0530

    commit 3

---
+++ mydjangoapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepageview ),  
+    path('home', views.homepageview, ),    
+    path('about', views.aboutview),
+    path('contact', views.contactview),
+    path('contactprocess', views.contactprocess),
+    path('shop', views.shoppageview),
+]
---
+++ mydjangoapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepageview(request):
+    return render(request, 'home.html')
+
+def aboutview(request):
+    return render(request, 'about.html')
+
+def contactview(request):
+    return render(request, 'contact.html')
+
+def shoppageview(request):
+    return render(request, 'shop.html')
+ 
+def contactprocess(request):
+    if request.method == 'POST':
+        a = request.POST['txt1']
+        b = request.POST['txt2']
+        msg = "your name is " , a , " and your email is " , b
+        return HttpResponse(msg)
+    
